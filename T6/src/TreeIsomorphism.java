f478c63a8505e1a0e0512ac8ef4315fb3aeaadb9
Átila Carvalho Rocha <atilacarvalhorocha@hotmail.com>
2026-04-29 17:29:19 -0300

    feat: isTree() and getValidationMessage()

--- T6/src/TreeIsomorphism.java
+++ T6/src/TreeIsomorphism.java
@@ -13,11 +13,20 @@ public class TreeIsomorphism {
     }
 
     public boolean isTree() {
-        throw new UnsupportedOperationException("TODO: validar se o grafo representa uma arvore");
+        int V = graph.V();
+        int E = graph.E();
+        return V > 0 && E == V - 1;
+        /*
+        * TODO: Adicionar DFS/BFS para validar a conexidade do grafo
+        *  Motivo: Pode ser que no dataset tenha duas arvores e que E = V - 1
+        * */
     }
 
     public String getValidationMessage() {
-        throw new UnsupportedOperationException("TODO: descrever o resultado da validacao da entrada");
+        if (isTree()){
+            return  "É uma árvore válida.";
+        }
+        else return "Não é umma árvore válida: Não cumpre a validação E = V - 1";
     }
 
     public int[] getCenters() {
